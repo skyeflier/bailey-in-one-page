a632d9f097b27353f7bd212c759b094b85e5cd07
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-05-01 23:24:46 -0500

    practicing code in index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+//ReactDOM.render allows me to render something that looks like HTML
+// What should I put on the screen, where should I put it
+ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
