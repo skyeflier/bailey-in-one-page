6321af8049b552b52802be0c90a9b7dd75c6253b
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-05-01 23:55:58 -0500

    practiced writing vanilla js declarative statements

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 //ReactDOM.render allows me to render something that looks like HTML
 // What should I put on the screen, where should I put it
-ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+
+ReactDOM.render(
+    <p>
+        <h1 className="header">Hello, React!</h1>
+        <ul>
+            <li>bullet one</li>
+            <li>bullet two</li>
+        </ul>
+    </p>,
+    document.getElementById("root"))
+
+    //Write declarative in vanilla javascript
+    // const h1 = document.createElement("h1")
+    // h1.textContent = "This is an imperative way to program"
+    // h1.className = "header"
+    // document.getElementById("root").append(h1)
